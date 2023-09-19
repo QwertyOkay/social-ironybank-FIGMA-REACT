91aa1410abf246065a84fdacee41a85b174b6a6d
Alexandr Kotenko <kpshmt@gmail.com>
2023-09-19 15:44:27 +0300

    гзвфну

--- src/components/homePageSections/Slots.jsx
+++ src/components/homePageSections/Slots.jsx
@@ -10,12 +10,6 @@ function Second() {
       <Container variant="containerMainSecond" className={styles.BG}>
         <div className={styles.textWrapSecond}>
           <h1 className={styles.title3}>Our Available Time Slots</h1>
-          <p className={styles.text4}>
-            Embark on an extraordinary gaming journey where state-of-the-art
-            visuals, captivating audio, and user-friendly gameplay converge. Our
-            slot games offer more than mere entertainment; they blend visuals
-            and sounds to transport you to a world of enchantment.
-          </p>
         </div>
         <div className={styles.imgWrapSecond}>
           <picture>
@@ -31,8 +25,14 @@ function Second() {
             />
             <img src={secondImage} alt="The Game" />
           </picture>
-          <h1 className={styles.title}>Obtain the App</h1>
+          {/* <h1 className={styles.title}>Obtain the App</h1> */}
         </div>
+        <p className={styles.text4}>
+          Embark on an extraordinary gaming journey where state-of-the-art
+          visuals, captivating audio, and user-friendly gameplay converge. Our
+          slot games offer more than mere entertainment; they blend visuals and
+          sounds to transport you to a world of enchantment.
+        </p>
       </Container>
     </Section>
   );
